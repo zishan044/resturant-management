01e9dd721e675dcd24a540e42159de9e221befe7
zishan044 <winchesterfelix007@gmail.com>
2023-05-06 21:18:40 +0600

    project structure laid out in mvc format

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"golang-resturant-management/middlewares"
+	"golang-resturant-management/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	port = os.Getenv(PORT)
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middlewares.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.InvoiceRoutes(router)
+	routes.MenuRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.OrderRoutes(router)
+	routes.TableRoutes(router)
+
+	router.Run(":" + port)
+
+}
