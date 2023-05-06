fd7f8f5fd74b25615057f2b84f6b3bcb91f7e437
zishan044 <winchesterfelix007@gmail.com>
2023-05-06 21:49:36 +0600

    routes defiend for menu router

--- routes/menuRouter.go
+++ routes/menuRouter.go
@@ -7,7 +7,7 @@ import (
 )
 
 func MenuRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.GET("menus/", controller.GetMenus())
+	incomingRoutes.GET("menus/", controller.GetMenus()) //get all menus
 	incomingRoutes.GET("menus/:menu_id", controller.GetMenu())
 	incomingRoutes.POST("menus", controller.CreateMenu())
 	incomingRoutes.PATCH("menus/:menu_id", controller.UpdateMenu())
