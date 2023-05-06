26ba8de0bcd9f8d59df4c0a07dae96018bf219d4
zishan044 <winchesterfelix007@gmail.com>
2023-05-07 01:56:03 +0600

    added function outlines for route handlers and helper functions

---
+++ controllers/menuController.go
@@ -0,0 +1,27 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetMenus() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func GetMenu() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func CreateMenus() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func UpdateMenu() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
