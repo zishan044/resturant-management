f09d67d192123f9bc4ae0e583338b6682f805bf6
zishan044 <winchesterfelix007@gmail.com>
2023-05-09 04:52:21 +0600

    middleware function update: Authentication middleware implemented

--- middlewares/authMiddleware.go
+++ middlewares/authMiddleware.go
@@ -1,9 +1,34 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	"golang-resturant-management/helpers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Authentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		token := c.Request.Header.Get("token")
 
+		if token == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "no authentication token provided"})
+			c.Abort()
+			return
+		}
+
+		claims, err := helpers.ValidateToken(token)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.First_name)
+		c.Set("last_name", claims.Last_name)
+		c.Set("uid", claims.Uid)
+
+		c.Next()
 	}
 }
